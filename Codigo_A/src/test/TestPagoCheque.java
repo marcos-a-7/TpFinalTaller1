5132d3d398fe605a4a24df5f2be39cfa09de837f
Marcos <marcos.aguilera.7.13@gmail.com>
2020-12-15 17:37:19 -0300

    Correccion en pago cheque

--- Codigo_A/src/test/TestPagoCheque.java
+++ Codigo_A/src/test/TestPagoCheque.java
@@ -15,13 +15,13 @@ public class TestPagoCheque {
 	public void testPorcentajeFisica() {
 		PagoCheque pagoCheque = new PagoCheque();
 		double porcentajeFisica = pagoCheque.porcentajeFisica();
-		Assert.assertEquals("El porcentaje para la persona fisica es correcto", 1.10, porcentajeFisica);
+		assert porcentajeFisica == 1.10 : "El porcentaje para la persona es incorrecto";
 	}
 	
 	@Test
 	public void porcentajeJuridica() {
 		PagoCheque pagoCheque = new PagoCheque();
 		double porcentajeJuridica = pagoCheque.porcentajeJuridica();
-		Assert.assertEquals("El porcentaje para la persona juridica es correcto", 1.15, porcentajeJuridica);
+		assert porcentajeJuridica == 1.15 : "El porcentaje para la persona es incorrecto";
 	}
 }
